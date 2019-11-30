c6224ec8fedbc07f1ec2935c498acdc79c8c180a
igjit <igjit1@gmail.com>
2019-11-30 17:23:04 +0900

    Read constant_pool

--- R/read_class.R
+++ R/read_class.R
@@ -2,6 +2,15 @@ name_lookup <- function(v) {
   function(x) names(which(v == x))
 }
 
+cp_tags <- c(CONSTANT_Utf8 = 1,
+             CONSTANT_Class = 7,
+             CONSTANT_String = 8,
+             CONSTANT_Fieldref = 9,
+             CONSTANT_Methodref = 10,
+             CONSTANT_NameAndType = 12)
+
+cp_tag_name_of <- name_lookup(cp_tags)
+
 read_class <- function(con) {
   if (is.character(con)) {
     con <- file(con, "rb")
@@ -11,10 +20,34 @@ read_class <- function(con) {
   magic <- readBin(con, "raw", 4, NA, FALSE, "big")
   minor_version <- read_u2(con)
   major_version <- read_u2(con)
+  constant_pool_count <- read_u2(con)
+  constant_pool <- replicate(constant_pool_count - 1, read_cp_info(con), simplify = FALSE)
 
   list(magic = magic,
        minor_version = minor_version,
-       major_version = major_version)
+       major_version = major_version,
+       constant_pool = constant_pool)
 }
 
+read_u1 <- function(con) readBin(con, "integer", 1, 1, FALSE, "big")
+
 read_u2 <- function(con) readBin(con, "integer", 1, 2, FALSE, "big")
+
+read_cp_info <- function(con) {
+  tag <- read_u1(con)
+  tag_name <- cp_tag_name_of(tag)
+  info <- switch(tag_name,
+                 CONSTANT_Utf8 = {
+                   length <- read_u2(con)
+                   list(length = length,
+                        bytes = intToUtf8(readBin(con, "integer", length, 1, FALSE, "big")))
+                 },
+                 CONSTANT_Class = list(name_index = read_u2(con)),
+                 CONSTANT_String = list(string_index = read_u2(con)),
+                 CONSTANT_Fieldref = list(class_index = read_u2(con), name_and_type_index = read_u2(con)),
+                 CONSTANT_Methodref = list(class_index = read_u2(con), name_and_type_index = read_u2(con)),
+                 CONSTANT_NameAndType = list(name_index = read_u2(con),
+                                             descriptor_index = read_u2(con)))
+
+  c(tag = tag, info)
+}
